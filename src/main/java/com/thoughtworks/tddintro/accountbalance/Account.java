8d23e431d005373d5414f72e43876ea37c4b6a3e
Austin Garrard <wootasaurus@gmail.com>
2015-12-23 08:40:09 -0600

    Wrote test: should increase my balance when I deposit money
    Created initial Account class

---
+++ src/main/java/com/thoughtworks/tddintro/accountbalance/Account.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.tddintro.accountbalance;
+
+/**
+ * @author Austin Garrard
+ */
+public class Account {
+
+    public Account(int initialBalance) {
+
+    }
+
+    public void deposit(int amount) {
+
+    }
+
+    public int balance() {
+
+        return -1;
+    }
+}
--- src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/accountbalance/AccountTests.java
@@ -1,11 +1,18 @@
 package com.thoughtworks.tddintro.accountbalance;
 
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class AccountTests {
-//    @Test
-//    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-//
-//    }
-//
+    @Test
+    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
+        Account myAccount = new Account(100);
+        myAccount.deposit(50);
+        assertThat(myAccount.balance(), is(150));
+    }
+
 //    @Test
 //    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
 //
