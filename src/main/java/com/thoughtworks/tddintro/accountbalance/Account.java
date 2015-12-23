d44649f847163da0ab53e2a4b0173128272abf4a
Austin Garrard <wootasaurus@gmail.com>
2015-12-23 08:47:15 -0600

    Does not decrease my balance when I withdraw money and do not have enough to cover the withdrawal

--- src/main/java/com/thoughtworks/tddintro/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/accountbalance/Account.java
@@ -16,6 +16,10 @@ public class Account {
     }
 
     public void withdraw(int amount) {
+        if(amount > this.balance) {
+            return;
+        }
+
         this.balance -= amount;
     }
 
