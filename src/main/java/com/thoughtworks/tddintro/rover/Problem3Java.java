93fa9627c200c479bf189e2e5486d225ae282393
Austin Garrard <wootasaurus@gmail.com>
2015-12-27 19:08:00 -0600

    Extracted Rover file parsing from main method

--- src/main/java/com/thoughtworks/tddintro/rover/Problem3Java.java
+++ src/main/java/com/thoughtworks/tddintro/rover/Problem3Java.java
@@ -33,69 +33,11 @@ public class Problem3Java
         }
 
         /** INIT **/
-        //input parsing
         String fileName = args[0];
-        Scanner fileScanner = null;
-        Scanner lineScanner = null;
-        //map of the Martian plateau
-        GridMap map = null;
-        //the rovers on the plateau
-        ArrayList<Rover> roverList = new ArrayList<Rover>();
-        //the commands for each of the rovers
-        ArrayList<String> commandsList = new ArrayList<String>();
+        ArrayList<Rover> roverList = new ArrayList<>();
+        ArrayList<String> commandsList = new ArrayList<>();
+        if (parseRover(fileName, roverList, commandsList)) return;
 
-
-        /** READ THE FILE **/
-        try
-        {
-            fileScanner = new Scanner(new BufferedReader(new FileReader(fileName)));
-
-            //read the grid
-            int xMax = fileScanner.nextInt();
-            int yMax = fileScanner.nextInt();
-            if(xMax < 0 || yMax < 0)
-            {
-                System.out.println("Invalid grid dimenstions. Terminating.");
-                return;
-            }
-            map = new GridMap(xMax, yMax);
-
-            fileScanner.nextLine();
-            while(fileScanner.hasNextLine())
-            {
-                //read the rover
-                lineScanner = new Scanner(fileScanner.nextLine());
-                int xPos = lineScanner.nextInt();
-                int yPos = lineScanner.nextInt();
-                Rover.Orientation orientation = Rover.Orientation.valueOf(lineScanner.next());
-                Rover rover = new Rover(xPos, yPos, orientation, map);
-                roverList.add(rover);
-                if(!map.addRover(rover))
-                {
-                    System.out.println("Invalid starting position. Terminating.");
-                    return;
-                }
-
-                //read the commands
-                commandsList.add(new String(fileScanner.nextLine()));
-            }
-
-        }
-        catch(IOException e)
-        {
-            System.out.format("Invalid input file '%s'. Terminating.\n", fileName);
-            return;
-        }
-        catch(InputMismatchException e)
-        {
-            System.out.format("Invalid input file '%s'. Terminating.\n", fileName);
-            return;
-        }
-        catch(NoSuchElementException e)
-        {
-            System.out.format("Invalid input file '%s'. Terminating.\n", fileName);
-            return;
-        }
 
         /** GIVE COMMANDS TO THE ROVERS **/
         for(int i = 0; i < roverList.size(); i++)
@@ -138,5 +80,62 @@ public class Problem3Java
         {
             System.out.println(rover.toString());
         }
+    }
+
+    private static boolean parseRover(String fileName, ArrayList<Rover> roverList, ArrayList<String> commandsList) {
+        Scanner fileScanner;
+        GridMap map;
+        Scanner lineScanner;
+        try
+        {
+            fileScanner = new Scanner(new BufferedReader(new FileReader(fileName)));
+
+            //read the grid
+            int xMax = fileScanner.nextInt();
+            int yMax = fileScanner.nextInt();
+            if(xMax < 0 || yMax < 0)
+            {
+                System.out.println("Invalid grid dimenstions. Terminating.");
+                return true;
+            }
+            map = new GridMap(xMax, yMax);
+
+            fileScanner.nextLine();
+            while(fileScanner.hasNextLine())
+            {
+                //read the rover
+                lineScanner = new Scanner(fileScanner.nextLine());
+                int xPos = lineScanner.nextInt();
+                int yPos = lineScanner.nextInt();
+                Rover.Orientation orientation = Rover.Orientation.valueOf(lineScanner.next());
+                Rover rover = new Rover(xPos, yPos, orientation, map);
+                roverList.add(rover);
+                if(!map.addRover(rover))
+                {
+                    System.out.println("Invalid starting position. Terminating.");
+                    return true;
+                }
+
+                //read the commands
+                commandsList.add(fileScanner.nextLine());
+            }
+
+        }
+        catch(IOException e)
+        {
+            System.out.format("Invalid input file '%s'. Terminating.\n", fileName);
+            return true;
+        }
+        catch(InputMismatchException e)
+        {
+            System.out.format("Invalid input file '%s'. Terminating.\n", fileName);
+            return true;
+        }
+        catch(NoSuchElementException e)
+        {
+            System.out.format("Invalid input file '%s'. Terminating.\n", fileName);
+            return true;
+        }
+        return false;
     }
 }
